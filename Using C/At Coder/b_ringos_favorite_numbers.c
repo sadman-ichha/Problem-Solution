20e69894d04eeac3524869185a479f4d2240874c
Sadman Ichha <sadmanichha1@gmail.com>
2024-11-06 23:34:22 +0600

    added

---
+++ Using C/At Coder/b_ringos_favorite_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int D, N;
+
+    // Read inputs D and N
+    // scanf("%d %d", &D, &N);
+
+    // long long result;
+
+    // // Determine the N-th smallest integer based on D
+    // if (D == 0)
+    // {
+    //     // Not divisible by 100 (1, 2, 3, ...)
+    //     result = N; // Simply return N
+    // }
+    // else if (D == 1)
+    // {
+    //     // Divisible by 100 exactly once (100, 200, 300, ...)
+    //     result = 100 * N; // Return 100 times N
+    // }
+    // else if (D == 2)
+    // {
+    //     // Divisible by 100 exactly twice (10,000, 20,000, 30,000, ...)
+    //     result = 10000 * N; // Return 10,000 times N
+    // }
+
+    // Print the result
+    printf("lldresult");
+
+    return 0;
+}
