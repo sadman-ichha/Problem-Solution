185a682294013928df14f26014a32f783a92c1be
Sadman Ichha <sadmanichha1@gmail.com>
2025-05-07 07:53:13 +0600

    added: solid reactangle

--- basics/01pattern_printing/cpp/01solid_rectangle.cpp
+++ basics/01pattern_printing/cpp/01solid_rectangle.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   
-    
+int main()
+{
+    int rows = 3, cols = 3;
+    // Outer loop: Controls the number of rows
+    for (int i = 0; i < rows; i++){
+        // Inner loop: Controls the number of columns in each row
+        for (int j = 0; j < cols; j++){
+            cout << "* ";
+            
+        }
+        cout << endl; /// move to next line after each row 
+    }
+
     return 0;
 }
